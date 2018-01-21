6590acec4edfc153a003323410ae02fefc5c84b4
yinghuoai <964549765@qq.com>
2018-01-21 13:38:07 +0800

    经典的除0异常实验

---
+++ src/com/shiyanlou/course/AirExceptionTest.java
@@ -0,0 +1,35 @@
+package com.shiyanlou.course;
+
+public class AirExceptionTest {
+
+    public static void main(String[] args){
+
+        System.out.println("Example 1: -1.0/0 = "
+
+         +(-1.0/0));
+        //演示负浮点数除0
+
+        System.out.println("Example 2: +1.0/0 = "
+        +(+1.0/0));
+        //演示正浮点数除0
+
+        System.out.println("Example 3: -1/0 = "
+        +(-1/0));
+        //演示负整数除0
+
+        System.out.println("Example 4: +1/0 = "
+        +(+1/0));
+        //演示正整数除0
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
